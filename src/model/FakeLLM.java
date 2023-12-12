c497a67adb12053ad5f8482c98c996b4d289d632
Alvaro <Alvaro@DESKTOP-K1CNTJE>
2023-12-12 02:53:49 +0100

    Logica de FakeLLM
    Implementada logica de FakeLLM con metodos consistentes

--- src/model/FakeLLM.java
+++ src/model/FakeLLM.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -17,19 +18,24 @@ import java.util.List;
  */
 public class FakeLLM implements ILLM {
 
-    private final String[] frases = {"Me alegro", "Yo soy de Murcia, y tu?", "Estoy de acuerdo", "Y que opinas de la tortilla sin cebolla?"};
+    private final String[] respuestas = {"Me alegro", "Yo soy de Murcia, y tu?", "Estoy de acuerdo", "Y que opinas de la tortilla sin cebolla?", "jajaj si", "No coincido contigo", "Me parece bien"};
     private final String[] saludos = {"Hey, como va todo?", "Hola que tal", "Saludos compa"};
+    private final String[] refranes = {"Más vale tarde que nunca", "En boca cerrada no entran moscas", "No hay mal que por bien no venga", "Más vale prevenir que lamentar", "A quien madruga, Dios le ayuda"};
 
     public FakeLLM() {
     }
 
     @Override
     public String hablar(String input) {
-        if (input.equals("hola")) {
-            return saludos[1];
-        } else {
-            return frases[1];
+        Random randNum = new Random();
+        if (isRefran(input)) {
+            return refranes[randNum.nextInt(refranes.length)];
+        } else if (isSaludo(input)) {
+            return saludos[randNum.nextInt(saludos.length)];
         }
+
+        //Por defecto
+        return respuestas[randNum.nextInt(saludos.length)];
     }
 
     @Override
@@ -37,6 +43,27 @@ public class FakeLLM implements ILLM {
         return "Fake";
     }
 
+//Metodos boolean para comprobar el tipo de mensaje del usuario
+    public boolean isSaludo(String input) {
+        final String[] checks = {"hola", "buenos dias", "que tal", "saludos", "como estas", "hey", "como va", "buenas noches", "buenas tardes"};
+        return comprobarPalabraEnArray(input, checks);
+    }
+
+    public boolean isRefran(String input) {
+        final String[] checks = {"refran", "dicho", "proverbio", "dicho"};
+        return comprobarPalabraEnArray(input, checks);
+    }
     
+//Metodo para comprobar si un String input contiene un substring de un array
+//Se usa para buscar palabras clave en un mensaje introducido por el usuario
+//Devuelve true en caso afirmativo y false en caso negativo
+    private boolean comprobarPalabraEnArray(String input, String[] checks) {
+        for (String str : checks) {
+            if (input.toLowerCase().contains(str)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
